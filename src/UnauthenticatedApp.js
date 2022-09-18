99d1205c960417ca4e4188978458d99f1eb2ac8c
emachipana <emachipana0102@gmail.com>
2022-09-17 19:06:42 -0500

    ADD: render home page on unauthenticated app

--- src/UnauthenticatedApp.js
+++ src/UnauthenticatedApp.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import LoginPage from "./pages/login";
+import HomePage from "./pages/home";
 
 function UnauthenticatedApp() {
   return (
     <React.Fragment>
       <Navbar />
       <Routes>
-        <Route index path="/" element={<h1>This is home</h1>} />
+        <Route index path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </React.Fragment>
