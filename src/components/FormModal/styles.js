ded4c2e4a698a1686d95bbb60cdacdcf85a52ed5
emachipana <emachipana0102@gmail.com>
2022-10-08 00:12:34 -0500

    ADD: render image on edit product

--- src/components/FormModal/styles.js
+++ src/components/FormModal/styles.js
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 
 export const Section = styled.div`
   width: 100%;
+  position: relative;
   display: grid;
   grid-template-columns: 48% 48%;
   justify-content: space-between;
@@ -10,4 +11,24 @@ export const Section = styled.div`
     display: flex;
     flex-direction: column;
   }
+`;
+
+export const Image = styled.img`
+  width: fit-content;
+  max-height: 250px;
+  object-fit: cover;
+  transition: .3s ease-in;
+  cursor: pointer;
+  margin: 0 0.5rem;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 250px;
+  gap: 1rem;
 `;
