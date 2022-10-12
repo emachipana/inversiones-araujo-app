b6f2402c87faf7506430eebac0409c237eed22f6
emachipana <emachipana0102@gmail.com>
2022-10-12 12:13:36 -0500

    ADD: product page

---
+++ src/pages/client/products.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Spinner } from "reactstrap";
+import Categories from "../../components/Categories";
+import ProductCard from "../../components/ProductCard";
+import { get } from "../../services";
+import { Container } from "../admin/styles";
+
+function ProductsPage() {
+  const [products, setProducts] = useState([]);
+  const [backup, setBackup] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetch() {
+      const response = await get("products");
+      setProducts(response);
+      setBackup(response);
+      setIsLoading(false);
+    }
+
+    fetch();
+  }, []);
+  
+  return (
+    <>
+      <Categories
+        backup={backup}
+        setParent={setProducts}
+      />
+      <Container>
+        {
+          isLoading
+          ?
+          <Spinner />
+          :
+          products.map(product => (
+            <ProductCard 
+              key={product.id}
+              brand={product.marca}
+              name={product.name}
+              category={`${product.category_name} (${product.sub_category_name})`}
+              description={product.description}
+              image={product.photos[0].url}
+              stock={product.stock}
+            />
+          ))
+        }
+      </Container>
+    </>
+  );
+}
+
+export default ProductsPage;
