0958cb11fca33b1c9b71bef9e449d95d13b05385
emachipana <emachipana0102@gmail.com>
2022-10-14 18:47:37 -0500

    ADD: fixed buttons to quotations and whatsapp links

--- src/UnauthenticatedApp.js
+++ src/UnauthenticatedApp.js
@@ -1,23 +1,58 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/client/home";
 import LoginPage from "./pages/client/login";
 import ProductsPage from "./pages/client/products";
-import { Container } from "./AuthenticatedApp";
+import { Button, FixedContainer, IconStyle, PopMessage } from "./pages/client/styles";
+import { BsWhatsapp } from "react-icons/bs";
+import { FcDocument } from "react-icons/fc";
+import Footer from "./components/Footer";
 
 function UnauthenticatedApp() {
+  const location = useLocation();
+
   return (
-    <React.StrictMode>
+    <React.Fragment>
       <Navbar />
-      <Container>
-        <Routes>
-          <Route index path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-        </Routes>
-      </Container>
-    </React.StrictMode>
+      <FixedContainer
+        location={location.pathname}
+        whatsapp
+      >
+        <Button
+          whatsapp
+          href="https://api.whatsapp.com/send?phone=51990849369&text=Hola!%20necesito%20m%C3%A1s%20informaci%C3%B3n"
+          target="_blank"
+        >
+          <BsWhatsapp 
+            css={IconStyle}
+          />
+        </Button>
+      </FixedContainer>
+      <FixedContainer
+        location={location.pathname}      
+      >
+        <Button
+          onClick={() => console.log("should be open quotation request view")}
+        >
+          <FcDocument 
+            css={IconStyle}
+          />
+        </Button>
+        <PopMessage>
+          Cotizar
+        </PopMessage>
+      </FixedContainer>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+      <Footer 
+        location={location}
+      />
+    </React.Fragment>
   );
 }
 
