feef582db1d8ff4c10b18ca181ba37b833809b68
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-14 10:58:51 -0500

    ADD: data context

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import AdminApp from "./AdminApp";
 import AgroinvitroApp from "./AgroinvitroApp";
 import MainApp from "./MainApp";
+import { DataProvider } from "./context/data";
 
 function App() {
   return (
-    <Routes>
-      <Route index path="/*" element={<MainApp />} />
-      <Route path="admin/*" element={<AdminApp />} />
-      <Route path="agroinvitro/*" element={<AgroinvitroApp />} />
-    </Routes>
+    <DataProvider>
+      <Routes>
+        <Route index path="/*" element={<MainApp />} />
+        <Route path="admin/*" element={<AdminApp />} />
+        <Route path="agroinvitro/*" element={<AgroinvitroApp />} />
+      </Routes>
+    </DataProvider>
   );
 }
 
