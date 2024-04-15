a924a9f1543d47a75271f1f194593f86109f5f33
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-15 18:15:19 -0500

    ADD: contact page

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Client/Home";
 import Footer from "./components/Footer";
 import Contact from "./pages/Client/Contact";
+import Services from "./pages/Client/Services";
 
 function ClientApp() {
   return (
@@ -12,7 +13,7 @@ function ClientApp() {
         <Route index exact path="/" element={<Home />} />
         <Route path="tienda" element={<h1>tienda page</h1>} />
         <Route path="tienda/producto/:id" element={<h1>tienda page producto detail</h1>} />
-        <Route path="servicios" element={<h1>services page</h1>} />
+        <Route path="servicios" element={<Services />} />
         <Route path="contactanos" element={<Contact />} />
         <Route path="nosotros" element={<h1>nostros page</h1>} />
         <Route path="carrito" element={<h1>carrito page</h1>} />
