39ccf81374c18929adddb2012777956ce6425a43
Enmanuel Chipana <emachipana0102@gmail.com>
2024-04-17 00:14:59 -0500

    FIX: use categories componenet on store page

--- src/pages/Client/Store/index.js
+++ src/pages/Client/Store/index.js
@@ -1,13 +1,11 @@
 import { useLocation } from "react-router-dom";
 import Banner from "../../../components/Banner";
 import { useData } from "../../../context/data";
-import { Categories, Container, Products, Wrapper } from "./styles";
+import { Container, Products } from "./styles";
 import { Title } from "../styles";
-import { Line } from "../../../components/Navbar/Menu/styles";
-import Item from "./Item";
-import InputRange from "./Range";
 import ProductCard from "../../../components/ProductCard";
 import Pagination from "../../../components/Pagination";
+import Categories from "../../../components/Categories";
 
 function Store() {
   const { search } = useLocation();
@@ -22,57 +20,9 @@ function Store() {
         withoutIcon
       />
       <Container>
-        <Categories>
-          <Wrapper>
-            <Title size={1.2}>CATEGORÍAS</Title>
-            <Line 
-              width={45}
-              style={{position: "relative"}}
-              />
-          </Wrapper>
-          <Item 
-            category="Todo"
-            isActive={category === "todo"}
-            num={69}
-            redirectTo="todo"
-          />
-          <Item 
-            category="Campo"
-            isActive={category === "campo"}
-            num={12}
-            redirectTo="campo"
-            withIcon
-          />
-          <Item 
-            category="Laboratorio"
-            isActive={category === "laboratorio"}
-            num={18}
-            redirectTo="laboratorio"
-            withIcon
-          />
-          <Item 
-            category="Invernadero"
-            isActive={category === "invernadero"}
-            num={30}
-            redirectTo="invernadero"
-            withIcon
-          />
-          <Item 
-            category="Riego tecnificado"
-            isActive={category === "riego"}
-            num={30}
-            redirectTo="riego"
-            withIcon
-          />
-          <Wrapper>
-            <Title size={1.2}>POR PRECIO</Title>
-            <Line 
-              width={50}
-              style={{position: "relative"}}
-              />
-          </Wrapper>
-          <InputRange min={35} max={950}/>
-        </Categories>
+        <Categories 
+          category={category}
+        />
         <Products>
           {
             isLoading
