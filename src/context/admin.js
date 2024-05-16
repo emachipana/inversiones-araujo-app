8dd8e993410e1bb6c4b2033cd58e67be1d15f403
Enmanuel Chipana <emachipana0102@gmail.com>
2024-05-16 10:52:22 -0500

    ADD: admin context

---
+++ src/context/admin.js
@@ -0,0 +1,61 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import apiFetch from "../services/apiFetch";
+
+const AdminContext = createContext();
+
+const AdminProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState({});
+  const [vitroOrders, setVitroOrders] = useState({});
+  const [events, setEvents] = useState([]);
+  const [orders, setOrders] = useState({});
+  const [messages, setMessages] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const orders = await apiFetch("orders");
+        setOrders(orders);
+        const vitroOrders = await apiFetch("vitro_orders");
+        setVitroOrders(vitroOrders);
+        setIsLoading(false);
+      }catch(e) {
+        console.error(e)
+        setIsLoading(false);
+        setError(e.message);
+      }
+    }
+
+    fetch();
+  }, []);
+
+  return (
+    <AdminContext.Provider
+      value={{
+        isLoading,
+        products,
+        vitroOrders,
+        events,
+        orders,
+        messages,
+        error,
+        setIsLoading,
+        setProducts,
+        setVitroOrders,
+        setEvents,
+        setOrders,
+        setMessages,
+        setError
+      }}
+    >
+      { children }
+    </AdminContext.Provider>
+  );
+}
+
+const useAdmin = () => {
+  return useContext(AdminContext);
+}
+
+export { AdminProvider, useAdmin };
