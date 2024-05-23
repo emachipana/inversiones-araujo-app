485dda5e9bcccf38d13f0734d4a420e4c569052c
Enmanuel Chipana <emachipana0102@gmail.com>
2024-05-23 00:51:12 -0500

    ADD: env variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,15 @@
 const BASE_API_URI = process.env.REACT_APP_BASE_API_URI;
 const TOKEN_KEY = process.env.REACT_APP_TOKEN_KEY;
-const RENIEC_URI = "https://dniruc.apisperu.com/api/v1/dni"
-const RENIEC_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVtYWNoaXBhbmEwMTAyQGdtYWlsLmNvbSJ9.PASC2puYZVgNH3Cu7RxCujrJgOAwu6i71-XfW4QHVQE";
+const RENIEC_URI = process.env.REACT_APP_RENIEC_URI;
+const RENIEC_TOKEN = process.env.REACT_APP_RENIEC_TOKEN;
 
-export { BASE_API_URI, TOKEN_KEY, RENIEC_TOKEN, RENIEC_URI };
+const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+export { BASE_API_URI, TOKEN_KEY, RENIEC_TOKEN, RENIEC_URI, FIREBASE_CONFIG };
